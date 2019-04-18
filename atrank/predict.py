2cbd748cd7d5baf36e6bbafa4c0bb41f1b0a61a8
kuanke <asdqsczser@gmail.com>
2019-04-18 17:48:57 +0800

    predict

--- atrank/predict.py
+++ atrank/predict.py
@@ -1,31 +1,35 @@
 import tensorflow as tf
+import sys
 if __name__ == '__main__':
-    filename =''
+    filename ='../raw_data/nsh_task_predictset_%s.txt2'%(sys.argv[1])
+    outputname = 'predict_%s.txt'%(sys.argv[1])
     modelname ='save_path1'
     items_count = 62
+    f_out = open(outputname,'w')
     with tf.Session() as sess:
         tf.saved_model.loader.load(sess,[tf.saved_model.tag_constants.SERVING],export_dir=modelname)
-        u_t=tf.get_default_graph().get_tensor_by_name('u')
-        i_t=tf.get_default_graph().get_tensor_by_name('i')
-        i_week_t=tf.get_default_graph().get_tensor_by_name('i_week')
-        i_daygap_t=tf.get_default_graph().get_tensor_by_name('i_daygap')
-        hist_i_t=tf.get_default_graph().get_tensor_by_name('hist_i')
-        hist_i_week_t=tf.get_default_graph().get_tensor_by_name('hist_i_week')
-        hist_i_daygap_t=tf.get_default_graph().get_tensor_by_name('hist_i_daygap')
-        sl_t=tf.get_default_graph().get_tensor_by_name('sl')
-        is_training_t=tf.get_default_graph().get_tensor_by_name('is_training')
-        logits_t=tf.get_default_graph().get_tensor_by_name('logits')
+        u_t=tf.get_default_graph().get_tensor_by_name('u:0')
+        i_t=tf.get_default_graph().get_tensor_by_name('i:0')
+        i_week_t=tf.get_default_graph().get_tensor_by_name('i_week:0')
+        i_daygap_t=tf.get_default_graph().get_tensor_by_name('i_daygap:0')
+        hist_i_t=tf.get_default_graph().get_tensor_by_name('hist_i:0')
+        hist_i_week_t=tf.get_default_graph().get_tensor_by_name('hist_i_week:0')
+        hist_i_daygap_t=tf.get_default_graph().get_tensor_by_name('hist_i_daygap:0')
+        sl_t=tf.get_default_graph().get_tensor_by_name('sl:0')
+        is_training_t=tf.get_default_graph().get_tensor_by_name('is_training:0')
+        logits_t=tf.get_default_graph().get_tensor_by_name('logits:0')
         datas = open(filename,'r').read().split('\n')
-        for data in datas :
+        for data in datas[1:-1] :
+            data=data.replace(', ',',')
             u = [data.split(' ')[0]]*items_count
             i = list(range(1,items_count+1))
             i_week = [data.split(' ')[2].split(':')[1]]*items_count
-            i_daygap = [data.split(' ')[2].split(':')[2]]*items_count
+            i_daygap = [0]*items_count
             hist = data.split(' ')[1].split(',')
             hist_i = [[x.split(':')[0] for x in hist]]*items_count
             hist_i_week = [[x.split(':')[1] for x in hist]]*items_count
-            hist_i_daygap = [[x.split(':')[1] for x in hist]]*items_count
-            sl = len(hist)
+            hist_i_daygap = [[int((int(data.split(' ')[2].split(':')[2]) - int(x.split(':')[2]))/3600.0/24) for x in hist]]*items_count
+            sl = [len(hist)]*items_count
         
             res = sess.run(logits_t, feed_dict={
                 u_t: u,
@@ -38,4 +42,4 @@ if __name__ == '__main__':
                 sl_t: sl,
                 is_training_t: False,
             })
-            print(u,res)
+            print(u[0]+' '+','.join(res),file=f_out)
